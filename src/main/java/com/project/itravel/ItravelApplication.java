9752c1429ce397d366621ed4d31aea0c40adad18
federicodececco <federicodececco1@gmail.com>
2025-08-04 18:45:42 +0200

    init

---
+++ src/main/java/com/project/itravel/ItravelApplication.java
@@ -0,0 +1,13 @@
+package com.project.itravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/itravel/ItravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.itravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
